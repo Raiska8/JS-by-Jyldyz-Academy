f7f248bf1861b6e1484d115742c9bf9ee1e3b180
RAISA ABDRAI KYZY <87840244+Raiska8@users.noreply.github.com>
2021-12-04 23:05:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Raiska'); // vyvesti imya
+console.log('Raiska'  + ' ' + 'Kulova'); // vyvesti imia i FIO
+console.log(100 + 100); // vyvesti chisla
+console.log('100' + '100'); // sdes on prosto string stoka= kankatinatsia
+document.write('100 + 100=' + (100+100)); // dobavili resultat vyrajenia
